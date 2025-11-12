b76849340f8a22363fc255f504176b5652ee341c
ashley.pike <39139509+TheHylianLegend@users.noreply.github.com>
2025-11-12 16:35:32 -0500

    Add metadata comments to INSERT_MATCH.sql
    Added metadata comments for procedure documentation.

--- Phase2/Procedures/INSERT_MATCH.sql
+++ Phase2/Procedures/INSERT_MATCH.sql
@@ -1,3 +1,8 @@
+-- TITLE: Fosgate_ADD_MATCH
+-- AUTHOR: Jeffrey Fosgate
+-- DATE OF GITHUB COMMIT: 11/5/2025
+-- A simple stored procedure for creating a new entry matching two roommates together.
+
 DELIMITER $$
 CREATE PROCEDURE ADD_MATCH (
     IN M_MATCH_ID INT UNSIGNED,
