0f23e2e9835887838668a72ce1f4a6491494d234
soppriola <spriola1106@gmail.com>
2025-11-15 18:54:28 -0500

    Update SUBMIT_REVIEW.sql

--- Phase2/Procedures/SUBMIT_REVIEW.sql
+++ Phase2/Procedures/SUBMIT_REVIEW.sql
@@ -1,20 +1,19 @@
 -- TITLE: SUBMIT_REVIEW.sql
 -- AUTHOR: SOPHIA PRIOLA 
--- SUBMIT_REVIEW IS A PROCEDURE THAT ALLOWS USERS TO SUBMIT REVIEWS IN TERMS
--- OF PROPERTIES THEY HAVE PREVIOUSLY RENTED
--- USERS CAN RANK PROPERTIES OUT OF 5 STARS AND ADD COMMENTS 
+-- SUBMIT_REVIEW Allows a user to submit a review for a property they have rented
+-- Users can rate a property out of 5 stars and optionally add a comment
 
 DELIMITER $$
 
 CREATE PROCEDURE SUBMIT_REVIEW(
 	-- Variables included 
-	IN p_USER_ID INT UNSIGNED ,
-	IN p_PROPERTY_ID INT UNSIGNED , 
-    IN p_COMMENT VARCHAR(1000) , 
-    IN p_STARS FLOAT(2,1) UNSIGNED
+	IN p_USER_ID INT UNSIGNED , -- ID of the user submitting the review
+	IN p_PROPERTY_ID INT UNSIGNED , -- ID of the property being reviewed 
+    IN p_COMMENT VARCHAR(1000) , -- Optional comment
+    IN p_STARS FLOAT(2,1) UNSIGNED -- Rating score (0.0 to 5.0 stars)
     )
 BEGIN 
-	-- INSERT VALUES INTO 
+	-- Insert the new review into the REVIEW table
     INSERT INTO REVIEW(USER_ID, PROPERTY_ID, COMMENT, STARS)
     VALUES (p_USER_ID, p_PROPERTY_ID, p_COMMENT, p_STARS);
 END$$
