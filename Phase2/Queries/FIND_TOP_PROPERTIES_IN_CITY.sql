986477c43090bc688fe67f17dc234b40f814c193
soppriola <spriola1106@gmail.com>
2025-11-15 07:45:05 -0500

    Optimize FIND_TOP_PROPERTIES_IN_CITY query
    Updated SQL query to join with ADDRESS table and use AVG(R.STARS) instead of AVG(R.RATING).

--- Phase2/Queries/FIND_TOP_PROPERTIES_IN_CITY.sql
+++ Phase2/Queries/FIND_TOP_PROPERTIES_IN_CITY.sql
@@ -4,24 +4,26 @@
 
 SELECT 
     P.PROPERTY_ID,
-    P.CITY,
+    A.CITY,
     (
-        SELECT AVG(R.RATING)
+        SELECT AVG(R.STARS)  
         FROM REVIEW AS R
         WHERE R.PROPERTY_ID = P.PROPERTY_ID
     ) AS AVG_RATING
 FROM PROPERTY AS P
-WHERE P.CITY = 'Portland'
+JOIN ADDRESS AS A
+    ON P.ADDR_ID = A.ADDR_ID
+WHERE A.CITY = 'Portland'
 ORDER BY AVG_RATING DESC; -- Orders largest to smallest 
 
 -- After optimization, find the top rated properties in a city and order them from largest to smallest 
 SELECT 
     P.PROPERTY_ID,
-    P.CITY,
+    A.CITY,
     AVG(R.RATING) AS AVG_RATING
 FROM PROPERTY AS P
-JOIN REVIEW AS R
-    ON R.PROPERTY_ID = P.PROP_ID
+JOIN ADDRESS AS A
+    ON A.ADDRESS_ID = P.PROP_ID
 WHERE P.CITY = 'Portland'
 GROUP BY 
     P.PROPERTY_ID,
