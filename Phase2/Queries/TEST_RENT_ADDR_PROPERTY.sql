20a95d99bdcfc5413c83ed16299f5e143bb52249
TheHylianLegend <ohp2004@gmail.com>
2025-11-15 14:36:47 -0500

    Added test cases for query optimization

---
+++ Phase2/Queries/TEST_RENT_ADDR_PROPERTY.sql
@@ -0,0 +1,11 @@
+EXPLAIN ANALYZE
+SELECT a.City, a.State_Code, 
+       COUNT(*) AS NumProperties,
+       MIN(p.RENT_COST) AS MinRent,
+       MAX(p.RENT_COST) AS MaxRent
+FROM PROPERTY p
+JOIN ADDRESS a ON p.ADDR_ID = a.ADDR_ID
+WHERE p.RENT_COST BETWEEN 2500 AND 2600  -- very selective range
+GROUP BY a.City, a.State_Code
+ORDER BY NumProperties DESC
+LIMIT 10;
