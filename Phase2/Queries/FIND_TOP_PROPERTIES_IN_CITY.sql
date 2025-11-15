e0bf203ad37f56dea34bb8ea80d7a67cb389c990
soppriola <spriola1106@gmail.com>
2025-11-15 07:24:57 -0500

    Refactor FIND_TOP_PROPERTIES_IN_CITY.sql for optimization
    Optimized query to find top rated properties in a city.

---
+++ Phase2/Queries/FIND_TOP_PROPERTIES_IN_CITY.sql
@@ -0,0 +1,29 @@
+-- TITLE: FIND_TOP_RATED_PROPS_IN_CITY
+-- AUTHOR: Sophia Priola
+-- Before optimization, find the top rated properties in a city and order them from largest to smallest 
+
+SELECT 
+    P.PROPERTY_ID,
+    P.CITY,
+    (
+        SELECT AVG(R.RATING)
+        FROM REVIEW AS R
+        WHERE R.PROPERTY_ID = P.PROPERTY_ID
+    ) AS AVG_RATING
+FROM PROPERTY AS P
+WHERE P.CITY = 'Portland'
+ORDER BY AVG_RATING DESC; -- Orders largest to smallest 
+
+-- After optimization, find the top rated properties in a city and order them from largest to smallest 
+SELECT 
+    P.PROPERTY_ID,
+    P.CITY,
+    AVG(R.RATING) AS AVG_RATING
+FROM PROPERTY AS P
+JOIN REVIEW AS R
+    ON R.PROPERTY_ID = P.PROP_ID
+WHERE P.CITY = 'Portland'
+GROUP BY 
+    P.PROPERTY_ID,
+    P.CITY
+ORDER BY AVG_RATING DESC; -- Orders by largest to smallest 
