d7451656e69dd3deb818255bff07601c9023633b
TheHylianLegend <ohp2004@gmail.com>
2025-11-21 15:11:57 -0500

    Updated SignUp.jsx with form functionality

--- mainepadfinder-app/backend/app.py
+++ mainepadfinder-app/backend/app.py
@@ -1,12 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from flask_cors import CORS
+import mysql.connector
+import re
 
 app = Flask(__name__)
 CORS(app)  # allows frontend to communicate with backend
 
-@app.get("/api/hello")
-def hello():
-    return {"message": "Hello from Flask!"}
+db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "pass",
+    database = "mydb"
+)
+
+@app.post("/api/signup")
+def signup():
+    data = request.get_json()
+    email = data["email"]
+    username = data["username"]
+    
+
+@app.get("/api/login")
+def login():
+    data = request.get_json()
+    username = data["username"]
+    password = data["password"]
 
 if __name__ == "__main__":
     app.run(debug=True)
