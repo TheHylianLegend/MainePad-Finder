fb77209e3f9d54bf936d40efa5682553b77c5c03
TheHylianLegend <ohp2004@gmail.com>
2025-11-21 15:11:57 -0500

    Updated SignUp.jsx with form functionality

--- mainepadfinder-app/frontend/src/pages/SignUp.jsx
+++ mainepadfinder-app/frontend/src/pages/SignUp.jsx
@@ -1,7 +1,102 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function SignUp() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [gender, setGender] = useState("M");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (password != confirmPassword) {
+      setError("Passwords do not match")
+      return;
+    }
+
+    try {
+      const response = await fetch ("http://127.0.0.1:5000/api/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, username, password, displayName, gender, phoneNumber, birthDate}),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        navigate("/login");
+      } else {
+        setError(data.error || "Signup failed");
+      }
+    } catch (err) {
+      setError("Network error");
+    }
+  }
+
   return (
     <div>
-      <h1>Sign Up Page</h1>
+      <h2>Sign Up</h2>
+      {error && <p style={{color: "red"}}>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        </div>
+
+        <div>
+          <label>Username:</label>
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
+        </div>
+
+        <div>
+          <label>Password:</label>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        </div>
+
+        <div>
+          <label>Confirm Password:</label>
+          <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required />
+        </div>
+
+        <div>
+          <label>Phone Number:</label>
+          <input type="tel" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} required />
+        </div>
+
+        <div>
+          <label>Birth Date:</label>
+          <input type="date" value={birthDate} onChange={(e) => setPhoneNumber(e.target.value)} required /> 
+        </div>
+
+        <div>
+          <label>Display Name:</label>
+          <input type="text" value={displayName} onChange={(e) => setDisplayName(e.target.value)} required />
+        </div>
+
+        <div>
+          <label>Gender:</label>
+          <select value={gender} onChange={(e) => setGender(e.target.value)} required >
+            <option value="M">Male</option>
+            <option value="F">Female</option>
+            <option value="X">Other</option>
+            <option value="?">Prefer not to say</option>
+          </select>
+        </div>
+
+        <button type="submit">
+          Sign Up
+        </button>
+      </form>
     </div>
   );
 }
