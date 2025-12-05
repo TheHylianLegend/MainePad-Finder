df61c03b98e00d2fa5ec566ddf900ab01a54e774
soppriola <spriola1106@gmail.com>
2025-12-04 20:27:25 -0500

    Enhance comments in BEST_DEAL_PROPERTIES SQL view
    Added comments to clarify the purpose of the view and its components.

--- SQL/Queries/BEST_DEAL_PROPS.sql
+++ SQL/Queries/BEST_DEAL_PROPS.sql
@@ -2,12 +2,14 @@
 -- AUTHOR: Sophia Priola
 -- This finds the best deals based on the city the user enters 
 -- by finding properties under the average for the city they filtered
+-- This is used to power the "Find me deals!" button in the 'Properties' page of our website
 
 USE MAINEPAD;
 
 -- We use view to build a derived table of the best deal properties
 CREATE OR REPLACE VIEW BEST_DEAL_PROPERTIES AS
 SELECT
+    -- our basic info 
     P.PROPERTY_ID,
     P.UNIT_LABEL,
     P.RENT_COST,
@@ -15,13 +17,17 @@ SELECT
     P.BATHROOMS,
     P.CAN_RENT,
     P.SQFT,
+    -- location info 
     A.CITY,
     A.STATE_CODE,
-    stats.avg_rent AS city_avg_rent,
-    ROUND((P.RENT_COST / stats.avg_rent) * 100, 0) AS rent_pct_of_city_avg
+    stats.avg_rent AS city_avg_rent,    -- average rent in this city 
+    ROUND((P.RENT_COST / stats.avg_rent) * 100, 0) AS rent_pct_of_city_avg    -- how this unit compares to city average as a percentage 
 FROM PROPERTY AS P
 JOIN ADDRESS AS A
   ON P.ADDR_ID = A.ADDR_ID
+
+-- our subquery calculates the average rent per city and only considers
+-- unit that are currently available (CAN_RENT = 0).
 JOIN (
     -- compute average rent per city for AVAILABLE units
     SELECT 
